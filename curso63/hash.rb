b09c9068541fa54c461a53e5e75e9f505ce3712e
alexis israel quezada palma <ipalm001@hotmail.com>
2021-07-14 11:36:35 -0400

    hash curso63

--- curso63/hash.rb
+++ curso63/hash.rb
@@ -1,1 +1,32 @@
-cl
+#hash
+# => hash rocket
+clientes={"nombre"=>"Juan", "apellido"=>"Perez","calle"=>"Prat ","numero"=>1234  }
+
+#consultar
+puts clientes["nombre"]
+puts clientes["numero"]
+#modificar
+clientes["calle"]="Arturo Prat"
+clientes["numero"]=2341
+#agregar
+clientes["email"]="jp@gmail.com"
+#eliminar
+clientes.delete("apellido")
+
+puts clientes.to_s
+
+#recorrer hash
+clientes.each do |llave,valor|
+    puts "llave: #{llave} => valor: #{valor} "
+end
+
+{
+    "clientes"=>[
+        {"nombre"=>"Juan", "apellido"=>"Perez","calle"=>"Prat ","numero"=>1234  },
+        {"nombre"=>"Ana", "apellido"=>"Perez","calle"=>"Prat ","numero"=>1234  },
+        {"nombre"=>"Pedro", "apellido"=>"Perez","calle"=>"Prat ","numero"=>1234  },
+        {"nombre"=>"Maria", "apellido"=>"Perez","calle"=>"Prat ","numero"=>1234  },
+        
+    ]
+
+}
