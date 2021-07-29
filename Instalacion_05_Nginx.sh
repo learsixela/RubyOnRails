245b70dc2b30a80c67067ebbdab26258400c244f
alexis israel quezada palma <ipalm001@hotmail.com>
2021-07-28 21:37:23 -0400

    sites available y enabled

--- Instalacion_05_Nginx.sh
+++ Instalacion_05_Nginx.sh
@@ -18,3 +18,44 @@ sudo /etc/init.d/nginx start
 sudo fuser -k 80/tcp
 sudo fuser -k 443/tcp
 sudo systemctl status nginx
+#control+c para salir
+#ir a la carpeta
+cd /etc/nginx
+#creamos las carpetas
+sudo mkdir sites-available
+sudo mkdir sites-enabled
+#accedemos a la ruta
+cd conf
+#editamos el archivo
+sudo nano nginx.conf
+
+#agregar al archivo, antes de: 
+## another virtual host using mix of IP-, name-, and port-based configuration
+include /etc/nginx/sites-enabled/*;
+## control+o y enter para guardar y ctrl+x para salir
+
+#ir a la ruta
+cd /etc/nginx/sites-available
+#crear el archivo
+sudo touch rails.conf
+#editar archivo
+sudo nano rails.conf
+
+#pegar el contenido con SU IP PUBLICA DE AWS <public_ip_address>
+
+server {
+        listen 80; 
+        server_name 3.19.143.178;
+        passenger_enabled on; 
+        passenger_app_env development; 
+        root /var/www/sistemabase/public;
+        }
+
+#crear enlace simbolico
+sudo ln -s /etc/nginx/sites-available/rails.conf /etc/nginx/sites-enabled/rails.conf
+
+#reiniciamos nginx
+sudo systemctl restart nginx
+#validamos el estado
+sudo systemctl status nginx
+#control+c y enter para salir
